32f86593fe60884ed6c5e73a82fee7f34b083f22
Juan Ramos <eramos@coppel.com>
2022-08-08 05:59:07 -0600

    Funcionalidad para pintar cada letra de la frase que se va adivinando

--- Assets/GameHandler.cs
+++ Assets/GameHandler.cs
@@ -10,6 +10,7 @@ public class GameHandler : MonoBehaviour
     void Start()
     {
         const string URL = "https://game2dbackend-alfa.herokuapp.com/test-phrase";
+        Screen.fullScreen = !Screen.fullScreen;
         Text txtPhraseText = transform.Find("PhraseText").GetComponent<Text>();
         //txtReturn.text = text;
         StartCoroutine(ProcessRequest(URL, txtPhraseText));
@@ -17,6 +18,8 @@ public class GameHandler : MonoBehaviour
 
     private IEnumerator ProcessRequest(string uri, Text txtPhraseText)
     {
+        string textPhrase = "";
+        string textPhraseHide = "";
         using (UnityWebRequest request = UnityWebRequest.Get(uri))
         {
             yield return request.SendWebRequest();
@@ -27,8 +30,24 @@ public class GameHandler : MonoBehaviour
             }
             else
             {
+                //Debug.Log(request.downloadHandler.text);
+                //txtPhraseText.text = request.downloadHandler.text;
                 Debug.Log(request.downloadHandler.text);
-                txtPhraseText.text = request.downloadHandler.text;
+                textPhrase = request.downloadHandler.text;
+                foreach(char c in textPhrase)
+                    {
+                        Debug.Log(c);
+                        if (c != ' '){
+                            textPhraseHide = textPhraseHide + "_";
+                        }
+                        else{
+                            textPhraseHide = textPhraseHide + " ";
+                        }
+                    }
+
+                txtPhraseText.text = textPhraseHide;
+                Text txtPhraseHide = GameObject.FindWithTag("PhraseTextHide").GetComponent<Text>();
+                txtPhraseHide.text = textPhrase;
             }
         }
     }
